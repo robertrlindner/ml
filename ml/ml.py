17de6daf14c2713443aa0c1f28805c7a3b39a19b
Robert Lindner <robertrlindner@gmail.com>
2014-09-29 23:20:08 -0500

    Added regression models

--- ml/ml.py
+++ ml/ml.py
@@ -11,16 +11,30 @@ from sklearn.ensemble import GradientBoostingClassifier
 def mean_keys(dict_list):
     return dict([(key,np.mean([dic[key] for dic in dict_list])) for key in dict_list[0]])
 
-def score(x, y, val='precision', tag=''):
-
-    if len(x)!=len(y):
-        print 'Arrays need to be the same size.'
-        return
+def rscore(x, y, tag=''):
 
     if tag: tag = '-'+tag
-
     x = np.array(x)
     y = np.array(y)
+
+    MSE = np.mean( (x-y)**2 )
+    MAFE = np.mean( np.abs((x-y)/y) )
+    MAD = np.mean(x-y)
+
+    out = dict([('MSE'+tag,MSE),('MAD'+tag,MAD),('MAFE'+tag,MAFE)])
+
+    return out
+
+
+
+
+
+def cscore(x, y, tag=''):
+
+    if tag: tag = '-'+tag
+    x = np.array(x)
+    y = np.array(y)
+
     TP = 1.0 * np.sum( (x == 1)  & (y == 1) )
     TN = 1.0 * np.sum( (x == 0)  & (y == 0) )
     FP = 1.0 * np.sum( (x == 1)  & (y == 0) )
@@ -37,10 +51,11 @@ def score(x, y, val='precision', tag=''):
         F1 = 2. * precision * recall / (precision + recall)
     out = dict([('Acc'+tag,acc),('F1'+tag,F1),('precision'+tag,precision),('recall'+tag,recall),
                  ('TP'+tag,TP),('TN'+tag,TN),('FP'+tag,FP),('FN'+tag,FN),('likelihood'+tag,likelihood)])
+    
     return out
 
 
-def NFold(X, Y, n_folds = 4, val='precision', decision=0.5, estimator=None):
+def NFold(X, Y, n_folds = 4, decision=0.5, estimator=None, classification=True):
 
     m = len(Y)
     factor, remainder = divmod(m, n_folds)
@@ -48,6 +63,7 @@ def NFold(X, Y, n_folds = 4, val='precision', decision=0.5, estimator=None):
     CV_scores = [None for i in range(n_folds)]
     train_scores = [None for i in range(n_folds)]
     for i in range(n_folds):
+        print 'NFOLD: ', i+1
         # CV
         w1 = i * factor 
         w2 = (i+1) * factor if i != n_folds - 1 else (i+1) * factor + remainder
@@ -59,18 +75,33 @@ def NFold(X, Y, n_folds = 4, val='precision', decision=0.5, estimator=None):
         w_train = np.logical_not(w_CV)
         X_train = X[w_train]
         Y_train = Y[w_train]
-
         estimator.fit(X_train, Y_train)
-        cv_predictions = estimator.predict_proba(X_CV)[:,1] >= decision
-        train_predictions = estimator.predict_proba(X_train)[:,1] >= decision
 
-        CV_scores[i] = score(cv_predictions, Y_CV, val=val, tag='cv')
-        train_scores[i] = score(train_predictions, Y_train, val=val, tag='train')
+        # Compute predictions
+        if classification:
+            cv_predictions = estimator.predict_proba(X_CV)[:,1] >= decision
+            train_predictions = estimator.predict_proba(X_train)[:,1] >= decision
+        else:
+            cv_predictions = estimator.predict(X_CV)
+            train_predictions = estimator.predict(X_train)
 
-    print mean_keys(CV_scores)
-    print mean_keys(train_scores)
+
+        # Compute performance scores
+        score = cscore if classification else rscore
+        CV_scores[i] = score(cv_predictions, Y_CV, tag='cv')
+        train_scores[i] = score(train_predictions, Y_train, tag='train')
+
+
+
     out = {'mean_CV_scores':mean_keys(CV_scores),
           'mean_train_scores':mean_keys(train_scores)}
+
+    # Format the output to 2 sigfigs
+    for top_key in ['mean_CV_scores', 'mean_train_scores']:
+        for key in out[top_key]:
+            out[top_key][key] = '{0:3.2f}'.format(out[top_key][key])
+
+
     return out
 
 if __name__=='__main__':
